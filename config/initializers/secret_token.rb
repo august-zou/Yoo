5026359dce31fc32ea7f7d88cf85b540cfe57093
august-zou <zjzouxu@gmail.com>
2012-11-12 13:18:40 +0800

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yoo::Application.config.secret_token = 'f25a0cf2506b51894c60c014b97cd8fa5ac7b8b1191a57fe9ab19ea4a1f2cfb5ffdbdc91a50592663a20e7fbd88d01c5737ccd244575687f838e1042577c6a20'
